13e40c6e7dddd68e539b017379117d05f7280f39
yosef6607 <117655867+yosef6607@users.noreply.github.com>
2022-12-02 20:30:20 +0300

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.py
+++ Reverse a String - GFG/reverse-a-string.py
@@ -1,12 +1,19 @@
 #User function Template for python3
 
 def reverseWord(s):
-    #your code here
-    string1 = ""
-    for i in range(len(s)-1,-1,-1):
-        string1 += s[i]
-    return string1
-        
+    list1 = list()
+    for i in s:
+        list1.append(i)
+    for i in range(len(list1)//2):
+        start_element =list1[i]
+        last_element = list1[(len(list1)-1) - i]
+        temp = start_element
+        start_element = last_element
+        last_element = temp
+        list1[i] = start_element
+        list1[(len(list1)-1) - i] = last_element
+    reverse_string = "".join(str(x) for x in list1)
+    return reverse_string
 
 
 #{ 
